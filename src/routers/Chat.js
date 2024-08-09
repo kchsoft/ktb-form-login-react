73eb9ccd1d25494fcd9c3e0d478809aab6e39ac7
kchranger <kchranger@gmail.com>
2024-08-09 15:33:03 +0900

    modify logout redirect

--- src/routers/Chat.js
+++ src/routers/Chat.js
@@ -1,10 +1,19 @@
 function Chat() {
 
     const logout = async () => {
-        fetch("http://localhost:8080/logout",{
-            method: "POST",
-            credentials: 'include',
-        })
+        try {
+            const response = await fetch("http://localhost:8080/logout", {
+                method: "POST",
+                credentials: 'include',
+            });
+
+            const json = await response.json();
+            alert("로그아웃 성공")
+            // 클라이언트 측에서 리디렉션 처리
+            window.location.href = json.redirectUrl;
+        } catch (error) {
+            console.error("Error during logout:", error);
+        }
     }
 
     return (
